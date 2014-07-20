65dfe0321acd36114d64e2c96825e87a774de41b
Σndless <loli.con@qq.com>
2014-07-20 15:19:22 +0800

    Update more02.c

--- more02.c
+++ more02.c
@@ -2,54 +2,53 @@
  *	read and print 24 lines then pause for a few special commands
  *      feature of version 0.2: reads from /dev/tty for commands
  */
-#include	<stdio.h>
+#include <stdio.h>
 
-#define	PAGELEN	24
-#define	LINELEN	512
+const int PAGELEN = 24;
+const int LINELEN = 512
 
 void do_more(FILE *);
 int see_more(FILE *);
 
-int main( int ac , char *av[] )
+int main(int ac , char *av[])
 {
-	FILE	*fp;
-
-	if ( ac == 1 )
-		do_more( stdin );
+	FILE *fp;
+	if (ac == 1)
+		do_more(stdin);
 	else
-		while ( --ac )
-			if ( (fp = fopen( *++av , "r" )) != NULL )
+		while (--ac)
+			if ((fp = fopen(*++av , "r")) != NULL)
 			{
-				do_more( fp ) ; 
-				fclose( fp );
+				do_more(fp) ;
+				fclose(fp);
 			}
 			else
 				exit(1);
 	return 0;
 }
 
-void do_more( FILE *fp )
+void do_more(FILE *fp)
 /*
  *  read PAGELEN lines, then call see_more() for further instructions
  */
 {
-	char	line[LINELEN];
+	char line[LINELEN];
 	int	num_of_lines = 0;
 	int	see_more(FILE *), reply;
-	FILE	*fp_tty;
-
-	fp_tty = fopen( "/dev/tty", "r" );	   /* NEW: cmd stream   */
-	if ( fp_tty == NULL )			   /* if open fails     */
+	FILE *fp_tty;
+	fp_tty = fopen("/dev/tty", "r");	     /* NEW: cmd stream   */
+	if (fp_tty == NULL)			     /* if open fails     */
 		exit(1);                           /* no use in running */
-
-	while ( fgets( line, LINELEN, fp ) ){		/* more input	*/
-		if ( num_of_lines == PAGELEN ) {	/* full screen?	*/
+	while (fgets(line, LINELEN, fp)) 		/* more input	*/
+	{
+		if (num_of_lines == PAGELEN)  	/* full screen?	*/
+		{
 			reply = see_more(fp_tty);  /* NEW: pass FILE *  */
-			if ( reply == 0 )		/*    n: done   */
+			if (reply == 0)		/*    n: done   */
 				break;
 			num_of_lines -= reply;		/* reset count	*/
 		}
-		if ( fputs( line, stdout )  == EOF )	/* show line	*/
+		if (fputs(line, stdout)  == EOF)	/* show line	*/
 			exit(1);			/* or die	*/
 		num_of_lines++;				/* count it	*/
 	}
@@ -61,17 +60,16 @@ int see_more(FILE *cmd)				   /* NEW: accepts arg  */
  *	q means no, space means yes, CR means one line
  */
 {
-	int	c;
-
+	char c;
 	printf("\033[7m more? \033[m");		/* reverse on a vt100	*/
-	while( (c=getc(cmd)) != EOF )		/* NEW: reads from tty  */
+	while ((c = getc(cmd)) != EOF)		/* NEW: reads from tty  */
 	{
-		if ( c == 'q' )			/* q -> N		*/
+		if (c == 'q')			/* q -> N		*/
 			return 0;
-		if ( c == ' ' )			/* ' ' => next page	*/
+		if (c == ' ')			/* ' ' => next page	*/
 			return PAGELEN;		/* how many to show	*/
-		if ( c == '\n' )		/* Enter key => 1 line	*/
-			return 1;		
+		if (c == '\n')		/* Enter key => 1 line	*/
+			return 1;
 	}
 	return 0;
 }
